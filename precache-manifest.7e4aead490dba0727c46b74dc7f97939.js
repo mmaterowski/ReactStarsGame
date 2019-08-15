3bc247581040de4eb93218898ce40f296b9b4045
michal materowski <michal.materowski@gmail.com>
2019-08-15 19:36:16 +0200

    Updates

---
+++ precache-manifest.7e4aead490dba0727c46b74dc7f97939.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dae145e6dca25fcd2cbb5b2d00b0a94",
+    "url": "/ReactStarsGame/index.html"
+  },
+  {
+    "revision": "beb1921a9f50f61f0708",
+    "url": "/ReactStarsGame/static/css/main.c4e9e033.chunk.css"
+  },
+  {
+    "revision": "e9460aa21fbe918bf515",
+    "url": "/ReactStarsGame/static/js/2.54aa09c5.chunk.js"
+  },
+  {
+    "revision": "beb1921a9f50f61f0708",
+    "url": "/ReactStarsGame/static/js/main.decbc282.chunk.js"
+  },
+  {
+    "revision": "b9234532f34d1bcb2e3a",
+    "url": "/ReactStarsGame/static/js/runtime~main.c8beb25b.js"
+  }
+]);
