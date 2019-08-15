55aeda09b473b45d99c0bcd7546fd6e403c087fe
michal materowski <michal.materowski@gmail.com>
2019-08-15 10:59:28 +0200

    changing app structure, extracting components

--- src/index.js
+++ src/index.js
@@ -1,10 +1,72 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const PlayNumber = props => (
+    <button className="number" onClick={() => console.log('Num', props.number)}>{props.number}</button>
+)
+
+const StarsDisplay = props => (
+    <>
+        {utils.range(1, props.count).map(starId =>
+            <div key={starId} className="star" />)}
+    </>
+)
+const StarMatch = () => {
+    const [stars, setStars] = React.useState(utils.random(1, 9));
+    return (
+        <div className="game">
+            <div className="help">
+                Pick 1 or more numbers that sum to the number of stars
+        </div>
+            <div className="body">
+                <div className="left">
+                    <StarsDisplay count={stars} />
+                </div>
+                <div className="right">
+                    {utils.range(1, 9).map(number =>
+                        <PlayNumber key={number} number={number} />
+                    )}
+                </div>
+            </div>
+            <div className="timer">Time Remaining: 10</div>
+        </div>
+    );
+};
+
+const colors = {
+    available: 'lightgray',
+    used: 'lightgreen',
+    wrong: 'lightcoral',
+    candidate: 'deepskyblue',
+};
+
+const utils = {
+    sum: arr => arr.reduce((acc, curr) => acc + curr, 0),
+
+    range: (min, max) => Array.from({ length: max - min + 1 }, (_, i) => min + i),
+
+    random: (min, max) => min + Math.floor(max * Math.random()),
+
+    randomSumIn: (arr, max) => {
+        const sets = [[]];
+        const sums = [];
+        for (let i = 0; i < arr.length; i++) {
+            for (let j = 0, len = sets.length; j < len; j++) {
+                const candidateSet = sets[j].concat(arr[i]);
+                const candidateSum = utils.sum(candidateSet);
+                if (candidateSum <= max) {
+                    sets.push(candidateSet);
+                    sums.push(candidateSum);
+                }
+            }
+        }
+        return sums[utils.random(0, sums.length)];
+    },
+};
+
+ReactDOM.render(<StarMatch />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
